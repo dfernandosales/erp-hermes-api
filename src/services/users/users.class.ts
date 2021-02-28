5b338aa54680688d195a7f34dd94c15f8e04df1d
Diogo Fernando de Melo Sales <diogo@eurekalabs.com.br>
2021-02-27 22:46:45 -0300

    Protegendo a auto-exclusao do usuario. Protejentendo passwords e tambem possibilidando o undo delete. Melhorias no usuario

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,7 +1,8 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
+import { BaseService } from '../base';
 
-export class Users extends Service {
+export class Users extends BaseService {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
