25aba00ef7081435612980950b2bc32b3601e1fb
Gabriel Sandoli <gsaboto@gmail.com>
2021-04-06 20:50:21 -0300

    Criacao de testes do quarto, correcao do banco de quarto (tipo no banco estava errado)

--- src/models/builder/index.ts
+++ src/models/builder/index.ts
@@ -1,3 +1,4 @@
 export * from './Reserva.builder';
 export * from './ReservaHospede.builder';
 export * from './ReservaQuarto.builder';
+export * from './Quarto.builder';
--- test/services/quarto.test.ts
+++ test/services/quarto.test.ts
@@ -1,5 +1,8 @@
+import { Paginated } from '@feathersjs/feathers';
 import assert from 'assert';
 import app from '../../src/app';
+import { QuartoClass, QuartoBuilder } from '../../src/models/builder';
+import { QuartoModel } from '../../src/models/quarto.model';
 
 describe('\'quarto\' service', () => {
   it('registered the service', () => {
@@ -8,3 +11,37 @@ describe('\'quarto\' service', () => {
     assert.ok(service, 'Registered the service');
   });
 });
+
+describe('teste criacao de quarto caminho feliz', () => {
+  it('create quarto, categoria-quarto', async () =>{
+    const categoriaQuarto = await app.service("categoria-quarto").create({ nome: "Teste Builder", valor: 250 });
+    const obj: QuartoClass = new QuartoBuilder()
+      .setCategoriaQuartoId(categoriaQuarto.id)
+      .setNumero(1)
+      .setVacancia(true)
+      .build();
+    try{
+      const created: any = await app.service('quarto').create(obj);
+    } catch(error) {
+      console.log(error)
+    }
+    const quartos = (await app.service('quarto').find()) as Paginated<QuartoModel>;
+    assert(quartos.total > 0)
+  })
+
+  it('o numero do quarto deve ser 1', async () => {
+    const quartos = (await app.service('quarto').find()) as Paginated<QuartoModel>;
+    quartos.data[0].numero === 1;
+  })
+
+  it('a vacancia deve ser falsa', async()  => {
+    const quartos = (await app.service('quarto').find()) as Paginated<QuartoModel>;
+    quartos.data[0].vacancia === false;
+  })
+
+  it("deve conseguir mudar a vacancia", async () => {
+    const quartos = (await app.service('quarto').find()) as Paginated<QuartoModel>;
+    await app.service('quarto').patch(quartos.data[0].id, { vacancia: true })
+    assert(quartos.data[0].vacancia === true)
+  })
+})
